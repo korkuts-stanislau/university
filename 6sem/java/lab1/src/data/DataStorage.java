77a7043b65be861fab1cf22f55c064a5f0f1c6cb
Stanislau <korkuc.stanislav@gmail.com>
2021-01-27 21:19:51 +0300

    Wrote class for data manipulation.

--- 6sem/java/lab1/src/data/DataStorage.java
+++ 6sem/java/lab1/src/data/DataStorage.java
@@ -2,9 +2,13 @@ package data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import station.PhonePlan;
+import station.PhoneService;
 import users.Admin;
 import users.Subscriber;
 import users.User;
+import users.requests.DeclineServiceRequest;
+import users.requests.PhoneChangeRequest;
+import users.requests.Request;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -73,5 +77,65 @@ public class DataStorage {
         BufferedWriter phonePlansFile = new BufferedWriter(new FileWriter(path.commonPath + path.pathToPhonePlans));
 
         mapper.writeValue(phonePlansFile, phonePlans);
+    }
+
+    public List<PhoneService> getPhoneServices() throws Exception {
+        Path path = Path.getPath();
+        ObjectMapper mapper = new ObjectMapper();
+
+        BufferedReader phoneServicesFile = new BufferedReader(new FileReader(path.commonPath + path.pathToPhoneServices));
+        PhoneService[] services = mapper.readValue(phoneServicesFile, PhoneService[].class);
+
+        return Arrays.asList(services);
+    }
+
+    public void savePhoneServices(List<PhoneService> phoneServices) throws Exception {
+        Path path = Path.getPath();
+        ObjectMapper mapper = new ObjectMapper();
+
+        BufferedWriter phoneServicesFile = new BufferedWriter(new FileWriter(path.commonPath + path.pathToPhoneServices));
+
+        mapper.writeValue(phoneServicesFile, phoneServices);
+    }
+
+    public List<Request> getRequests() throws Exception {
+        Path path = Path.getPath();
+        ObjectMapper mapper = new ObjectMapper();
+
+        BufferedReader declineRequestsFile = new BufferedReader(new FileReader(path.commonPath + path.pathToDeclineServiceRequests));
+        DeclineServiceRequest[] declineServiceRequests = mapper.readValue(declineRequestsFile, DeclineServiceRequest[].class);
+
+        BufferedReader phoneChangeRequestsFile = new BufferedReader(new FileReader(path.commonPath + path.pathToPhoneChangeRequests));
+        PhoneChangeRequest[] phoneChangeRequests = mapper.readValue(phoneChangeRequestsFile, PhoneChangeRequest[].class);
+
+        List<Request> requests = new ArrayList<>();
+        requests.addAll(Arrays.asList(declineServiceRequests));
+        requests.addAll(Arrays.asList(phoneChangeRequests));
+
+        return requests;
+    }
+
+    public void saveRequests(List<Request> requests) throws Exception {
+        Path path = Path.getPath();
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<DeclineServiceRequest> declineServiceRequests = new ArrayList<>();
+        List<PhoneChangeRequest> phoneChangeRequests = new ArrayList<>();
+
+        for (Request request :
+                requests) {
+            if(request instanceof DeclineServiceRequest) {
+                declineServiceRequests.add((DeclineServiceRequest)request);
+            }
+            else if(request instanceof PhoneChangeRequest) {
+                phoneChangeRequests.add((PhoneChangeRequest)request);
+            }
+        }
+
+        BufferedWriter declineServiceRequestsFile = new BufferedWriter(new FileWriter(path.commonPath + path.pathToDeclineServiceRequests));
+        BufferedWriter phoneChangeRequestsFile = new BufferedWriter(new FileWriter(path.commonPath + path.pathToPhoneChangeRequests));
+
+        mapper.writeValue(declineServiceRequestsFile, declineServiceRequests);
+        mapper.writeValue(phoneChangeRequestsFile, phoneChangeRequests);
     }
 }
